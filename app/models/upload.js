3054d1a295edb2c6fbe9ae59af18b471c3662686
dwbergstrom <dwbergstrom@gmail.com>
2018-11-05 10:54:27 -0500

    Create upload model
    + has url and title
    + not fully tested
    TODO:  create routes to test

---
+++ app/models/upload.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const uploadSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+const Upload = mongoose.model('Upload', uploadSchema)
+Upload.create({title: 'newTitle', url: 'www.example.com'})
+  .then(upload => console.log(upload))
+module.exports = mongoose.model('Upload', uploadSchema)
